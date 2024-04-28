df85972cd5c4f6d8edacaebdfb32e4264b337b3c
chi-touch <gloebudav@gmail.com>
2024-04-28 12:45:12 +0100

    still on it

--- chapter4/CreditLimitCalculator.go
+++ chapter4/CreditLimitCalculator.go
@@ -1,8 +1,35 @@
 package main
 
-func main() {
+var accountNumber, balanceAtBeginning, charges, credits, credit int
+var newBalance int
 
-	var accountNumber, total of all items charged by the customer this month int
-	var balanceAtTheBeginningOfTheMonth,
+func getNewBalance(balanceAtBeginning, charges, credits int) int {
+	newBalance := balanceAtBeginning + charges - credits
+	return newBalance
+}
+
+func checkIfNewBalanceExceedCredit(balanceAtBeginning, charges, credits int) string {
+	newBalance := balanceAtBeginning + charges - credits
+	if credit > newBalance {
+		return "Credit limit exceeded"
+	}
+	return "Credit limit is not exceeded"
 
 }
+
+//fmt.Print("Please enter your account number: ")
+//fmt.Scanln(&accountNumber)
+//
+//fmt.Print("Please enter your balance at beginning: ")
+//fmt.Scanln(&balanceAtBeginning)
+//
+//fmt.Print("Enter total of all items charged by the customer this month: ")
+//fmt.Scanln(&charges)
+//
+//fmt.Print("Enter total of all credits applied to the customer’s account this month: ")
+//fmt.Scanln(&totalOfAllCredits)
+//
+//fmt.Print("Enter allowed credit limit: ")
+//fmt.Scanln(&credit)
+//
+//newBalance = balanceAtBeginning + charges - credit
---
+++ chapter4/creditLimitCalculator_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestGetNewBalance(t *testing.T) {
+	//	test := []struct {
+	//		accountNumber int
+	//
+	//		charges            int
+	//		credit             int
+	//		balanceAtBeginning int
+	//		newBalance         int
+	//		expectedResult     int
+	//	}{
+	//		{3506856011, 5, 50},
+	//	}
+	//
+}
