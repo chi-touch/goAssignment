137eede09cbe10dceaef8dbce4519e4deb49408b
chi-touch <gloebudav@gmail.com>
2024-04-28 14:40:15 +0100

    still on it

--- chapter4/CreditLimitCalculator.go
+++ chapter4/CreditLimitCalculator.go
@@ -1,35 +1,45 @@
 package main
 
-var accountNumber, balanceAtBeginning, charges, credits, credit int
-var newBalance int
+import "fmt"
 
-func getNewBalance(balanceAtBeginning, charges, credits int) int {
-	newBalance := balanceAtBeginning + charges - credits
-	return newBalance
-}
+//var accountNumber, balanceAtBeginning, charges, credits, credit int
+//var newBalance int
+//
+//func getNewBalance(balanceAtBeginning, charges, credits int) int {
+//	newBalance := balanceAtBeginning + charges - credits
+//	return newBalance
+//}
+//
+//func checkIfNewBalanceExceedCredit(balanceAtBeginning, charges, credits int) string {
+//	newBalance := balanceAtBeginning + charges - credits
+//	if credit > newBalance {
+//		return "Credit limit exceeded"
+//	}
+//	return "Credit limit is not exceeded"
+//
+//}
 
-func checkIfNewBalanceExceedCredit(balanceAtBeginning, charges, credits int) string {
-	newBalance := balanceAtBeginning + charges - credits
-	if credit > newBalance {
-		return "Credit limit exceeded"
+func main() {
+
+	var accountNumber, balanceAtBeginning, charges, credits int
+	var newBalance int
+
+	fmt.Print("Please enter your account number: ")
+	fmt.Scanln(&accountNumber)
+
+	fmt.Print("Please enter your balance at beginning: ")
+	fmt.Scanln(&balanceAtBeginning)
+
+	fmt.Print("Enter total of all items charged by the customer this month: ")
+	fmt.Scanln(&charges)
+
+	fmt.Print("Enter allowed credit limit: ")
+	fmt.Scanln(&credits)
+
+	newBalance = balanceAtBeginning + charges - credits
+
+	if newBalance > credits {
+		fmt.Print("Credit limit exceeded")
 	}
-	return "Credit limit is not exceeded"
 
 }
-
-//fmt.Print("Please enter your account number: ")
-//fmt.Scanln(&accountNumber)
-//
-//fmt.Print("Please enter your balance at beginning: ")
-//fmt.Scanln(&balanceAtBeginning)
-//
-//fmt.Print("Enter total of all items charged by the customer this month: ")
-//fmt.Scanln(&charges)
-//
-//fmt.Print("Enter total of all credits applied to the customer’s account this month: ")
-//fmt.Scanln(&totalOfAllCredits)
-//
-//fmt.Print("Enter allowed credit limit: ")
-//fmt.Scanln(&credit)
-//
-//newBalance = balanceAtBeginning + charges - credit
