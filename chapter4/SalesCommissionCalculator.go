bc9f2583789ebfc62c46113a94255d8d7bb0399b
chi-touch <gloebudav@gmail.com>
2024-04-28 20:15:58 +0100

    still on it

--- chapter4/SalesCommissionCalculator.go
+++ chapter4/SalesCommissionCalculator.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var commission, itemValue, value, salesPersonEarnings float64
+
+	for count := 0; count < math.MaxInt; count++ {
+
+		fmt.Println("Enter the value of the item: ")
+		fmt.Scanln(&itemValue)
+		value += itemValue
+
+		commission = 0.9 * value
+
+		fmt.Println("Do you want to add another item")
+		var prompt string
+		fmt.Scanln(&prompt)
+		if prompt == "yes" {
+			continue
+		} else {
+			break
+		}
+	}
+	salesPersonEarnings = 200 + commission
+
+	fmt.Println("this is your weekly wage", salesPersonEarnings)
+
+}
